53e5644ecc3e7f63c822d0d6fda189fcf906489c
RogerGN <roger.goulart.nascimento@gmail.com>
2024-03-12 14:47:05 -0300

    config project: nativewind, font, colors

---
+++ src/app/index/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const index = (props: Props) => {
+  return (
+    <View className="h-['100%'] flex justify-center items-center bg-3-DarkMuted">
+      <Text className="text-gray-50">index</Text>
+    </View>
+  )
+}
+
+export default index
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import { colors, fontFamily } from './src/styles'
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors,
+      fontFamily,
+    },
+  },
+  plugins: [],
+}
